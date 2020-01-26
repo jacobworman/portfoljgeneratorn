3f65089e396910e550889b41cb63d985aa1b019e
jacobworman <jacobworman@gmail.com>
2020-01-26 20:58:44 +0100

    jkf

---
+++ build/precache-manifest.7ad5904100c51d36835c8cb45938d5c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ddf0ba5a5404e0f5bbd4df0b0532fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf46fd3f084057d315f",
+    "url": "/static/css/2.5135d50e.chunk.css"
+  },
+  {
+    "revision": "d6d9ea4817f75942fd44",
+    "url": "/static/css/main.803d01ed.chunk.css"
+  },
+  {
+    "revision": "daf46fd3f084057d315f",
+    "url": "/static/js/2.2b0243a7.chunk.js"
+  },
+  {
+    "revision": "d6d9ea4817f75942fd44",
+    "url": "/static/js/main.9b49eb45.chunk.js"
+  },
+  {
+    "revision": "234c130d62d7071fdc58",
+    "url": "/static/js/runtime~main.884fb6c0.js"
+  },
+  {
+    "revision": "f8dccee1b753bc2969e7fe104a7ad4fb",
+    "url": "/static/media/blob-shape.f8dccee1.svg"
+  }
+]);
